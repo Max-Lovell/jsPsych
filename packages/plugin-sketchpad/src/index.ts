be9c4b0089f2c386091878cde0f41096d3aa0f4f
max-lovell <max_lovell@hotmail.co.uk>
2024-08-06 18:19:45 +0100

    Array.prototype.slice.call() -> traditional for loop

--- packages/plugin-sketchpad/src/index.ts
+++ packages/plugin-sketchpad/src/index.ts
@@ -461,9 +461,9 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
       this.display.querySelector("#sketchpad-clear").addEventListener("click", this.clear);
     }
 
-    const color_btns = Array.prototype.slice.call(this.display.querySelectorAll(".sketchpad-color-select"));
-    for (const btn of color_btns) {
-      btn.addEventListener("click", (e) => {
+    const nodeList = this.display.querySelectorAll(".sketchpad-color-select");
+    for (let b = 0; b < nodeList.length; b++) {
+      nodeList[b].addEventListener("click", (e) => {
         const target = e.target as HTMLButtonElement;
         this.current_stroke_color = target.getAttribute("data-color");
       });
